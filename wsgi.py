18fdb2987666193ed6bbc154ed037031e46698c1
Sadul Khod <sadul.khod@riaadvisroy.com>
2022-05-28 20:56:04 +0530

    Change for default page.

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 model, mat_movies = util.pre_process()
 
 
+@app.route("/")
+def welcome():
+    return """Hello, Welcome to movie recommender api.
+    Please browse https://movie-recommander.herokuapp.com/movie/<movie_name>?count=<no_of_suggested_movies> link to get suggested movies."""
+
 @app.route("/movie/<movie>")
 def recommend_movie(movie):
     args = request.args
