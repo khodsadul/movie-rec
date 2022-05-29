f2990268039e77a3a200a62fa7223fefc4417b33
Sadul Khod <sadul.khod@riaadvisroy.com>
2022-05-29 16:49:55 +0530

    change for cors

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,24 @@
-import json
 import os
-
 import flask
 from flask import Flask, request, jsonify
-
 import util
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 model, mat_movies = util.pre_process()
 
 
 @app.route("/")
+@cross_origin()
 def welcome():
     return flask.render_template('welcome.html')
 
 
 @app.route("/movie/<movie>")
+@cross_origin()
 def recommend_movie(movie):
     args = request.args
     output = util.recommender(movie, mat_movies, model, args.get('count', default=10, type=int))
@@ -26,4 +28,3 @@ def recommend_movie(movie):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
-
